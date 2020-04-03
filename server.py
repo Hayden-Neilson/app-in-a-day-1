5c1954c12aa70015d296da949defd93b996aa5a2
AlexRoy96 <Alexroy96@outlook.com>
2020-04-03 12:03:06 -0600

    wolo

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ def home():
 @app.route("/return_info", methods=["POST"])
 def return_info():
     if request.method == "POST":
-       return "true"
+       return request.form
 
 
 if __name__ == "__main__":
